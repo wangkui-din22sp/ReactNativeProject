493469ff3802da04491a286be784444fb0d52036
Wang Kui <wyatt1115@gmail.com>
2024-01-17 13:34:18 +0200

    V1

--- App.tsx
+++ App.tsx
@@ -1,24 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
-import StyleDemo from './components/StyleDemo';
-import Dimensions1 from './components/1Dimensions';
-import ImageDemo from './components/ImageDemo';
+import ImageDemo from './components/Layout';
 
 
 export default function App() {
   let output
-  output = <ImageDemo image1={require('./assets/Presentation2.jpg')} text1="hahahahaha" 
-  text2="hahahahaha"
+  output = <ImageDemo image1={require('./assets/Presentation2.jpg')} text1="hahahahaha" text2="hahahahaha"
   image2={require('./assets/Presentation1.jpg')}  
   image3={require('./assets/BEAN.jpg')} text3="hahahahaha"
   />
   return output
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
